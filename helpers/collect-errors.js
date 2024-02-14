a66ba32343abd159fd9ea37de616c6330cbde87c
Sach7n <sachinmacwan07@gmail.com>
2024-02-13 22:30:45 -0500

    gql-6.3-added generic error when code is undefined

--- helpers/collect-errors.js
+++ helpers/collect-errors.js
@@ -1,15 +1,21 @@
 const errorsDict = [
   {
+    Description: "Unknown error occured, contact support team",
+    ErrorType: "INVALID",
+    Expected: null,
+    code: 1000,
+  },
+  ,{
     Description: "Bad Input. Please check input JSON.",
     ErrorType: "INVALID_JSON",
     Expected: "Valid JSON",
-    code: 1000,
+    code: 1001,
   },
   {
     Description: "Input values contains invalid field data",
     ErrorType: "INVALID_DATA",
     Expected: "Valid DATA",
-    code: 1001,
+    code: 1002,
   },
 ];
 
@@ -18,6 +24,9 @@ function collect_errors(errors) {
   let resultArray = [];
 
   resultArray = errors.map((e) => {
+    if(!e.code){
+      e.code = 1000;
+    }
     const errorMatch = errorsDict.find((error) => error.code === e.code);
 
     if (errorMatch) {
